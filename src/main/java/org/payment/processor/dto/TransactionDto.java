3975e8c37ad55cb7294ce128cb0fc94e26e81726
saurabh7517 <saurabh7517@gmail.com>
2025-12-27 08:30:33 +0000

    [tx-11] - Transaction Dto

---
+++ src/main/java/org/payment/processor/dto/TransactionDto.java
@@ -0,0 +1,40 @@
+package org.payment.processor.dto;
+
+public class TransactionDto {
+    private String transactionId;
+    private String senderAccount;
+    private String receiverAccount;
+    private double amount;
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getSenderAccount() {
+        return senderAccount;
+    }
+
+    public void setSenderAccount(String senderAccount) {
+        this.senderAccount = senderAccount;
+    }
+
+    public String getReceiverAccount() {
+        return receiverAccount;
+    }
+
+    public void setReceiverAccount(String receiverAccount) {
+        this.receiverAccount = receiverAccount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
